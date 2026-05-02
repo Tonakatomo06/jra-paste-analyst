bccff145d57a7d1f67ae819488605640b5a22935
Tonakatomo06 <tshnakamura06@gmail.com>
2026-05-02 13:29:36 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "jra-paste-analyst-v7";
+const CACHE_NAME = "jra-paste-analyst-v8";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=7",
-  "./app-v7.js?v=7",
+  "./styles.css?v=8",
+  "./app-v8.js?v=8",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
